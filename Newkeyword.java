b27380742e3e24972cdb01a3a5089c2be1d99ad1
Shuheb1995 <126678035+Shuheb1995@users.noreply.github.com>
2023-03-08 01:40:07 +0530

    Add files via upload

---
+++ Newkeyword.java
@@ -0,0 +1,25 @@
+public class Newkeyword{
+	public static void main(String[] args){
+	String[] roleModel= new String[6];
+	String[] vehicles= new String[8];
+	String[] institutes= new String[9];
+	String[] laptops= new String[20];
+	String[] chairs= new String[15];
+	String[] students= new String[25];
+	String[] girlsInClass= new String[6];
+	String[] boysInClass= new String[5];
+	String[] shops= new  String[30];
+	String[] cigerattes= new String[10];
+	System.out.println(roleModel[4]);
+	System.out.println(vehicles[2]);
+	System.out.println(institutes[3]);
+	System.out.println(laptops[10]);
+	System.out.println(chairs[11]);
+	System.out.println(students[19]);
+	System.out.println(girlsInClass[3]);
+	System.out.println(boysInClass[2]);
+	System.out.println(shops[25]);
+	System.out.println(cigerattes[1]);
+	}
+	
+}
---
+++ Sameliterals.java
@@ -0,0 +1,19 @@
+public class Sameliterals{
+	public static void main(String[] args){
+		String animal= "Elephant";
+		String bigAnimal= "Elephant";
+		System.out.println(animal==bigAnimal);
+		String sport= "Athletic";
+		String running= "Athletic";
+		System.out.println(sport==running);
+		String food= "Pasta";
+		String favFood= "Pasta";
+		System.out.println(food==favFood);
+		String actor= "Hrithik Roshan";
+		String greekGod= "Hrithik Roshan";
+		System.out.println(actor==greekGod);
+		String car= "4 Wheeler";
+		String carSwift= "4 Wheeler";
+		System.out.println(car==carSwift);
+	}
+}
