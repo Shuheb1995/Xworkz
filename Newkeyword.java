32a140ade749765ba295728795922d4e70a5fcf3
Shuheb1995 <shuheb.xworkz@gmail.com>
2023-03-07 13:17:37 +0530

    string code

---
+++ Newkeyword.java
@@ -0,0 +1,23 @@
+public class Newkeyword{
+	public static void main(String[] args){
+	String[] roleModel= new String[6];
+	String[] vehicles= new String[8];
+	String[] institutes= new String[9];
+	String[] laptops= new String[20];
+	String[] chairs= new String[15];
+	String[] students= new String[25];
+	String[] girlsInClass= new String[6];
+	String[] boysInClass= new String[5];
+	String[] shops= new  String[30];
+	System.out.println(roleModel[4]);
+	System.out.println(vehicles[4]);
+	System.out.println(institutes[4]);
+	System.out.println(laptops[4]);
+	System.out.println(chairs[4]);
+	System.out.println(students[4]);
+	System.out.println(girlsInClass[4]);
+	System.out.println(boysInClass[4]);
+	System.out.println(shops[4]);
+	}
+	
+}
